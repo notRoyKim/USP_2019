e305be873ae3281f36f1f556083a7546dd3b43ac
NRK <49558826+notRoyKim@users.noreply.github.com>
2019-05-21 21:12:31 +0900

    Update main.c
    made content type finder
    next,
    1. make +GET finder.
    2. add content type finder to my main() func.
    3. make this server using pthread.

--- main.c
+++ main.c
@@ -4,10 +4,45 @@
 #include<sys/stat.h>    
 #include<sys/types.h>    
 #include<unistd.h>
-
 #include <arpa/inet.h>
 #include <sys/un.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
 
+// char* find_get_name (char *buf)
+
+char* find_content_type (char *filename) {
+	char *p;
+	int i;
+	char buf1[256];
+	char *buf2;
+	char *token;
+
+	p = (char *)malloc(30);
+	strcpy(buf1, filename);
+
+	token = strtok(buf1, ".");
+	while(token != NULL)
+	{
+		buf2 = token;
+		token = strtok(NULL, ".");
+	}
+
+	if(strcmp(buf2, "html") == 0 || strcmp(buf2, "hml") ==  0)
+		strcpy(buf2, "Content-Type: text/html \r\n");
+	else if(strcmp(buf2, "txt") == 0)
+		strcpy(buf2, "Content-Type: text/plain \r\n");
+	else if(strcmp(buf2, "jpg") == 0 || strcmp(buf2, "jpeg") ==  0)
+		strcpy(buf2, "Content-Type: image/jpeg \r\n");
+	else if(strcmp(buf2, "gif") == 0)
+		strcpy(buf2, "Content-Type: image/gif \r\n");
+	else
+		strcpy(buf2, "Content-Type: application/octet-stream \r\n");
+
+	p = buf2;
+	printf("content-type: %s\n", p);
+	return p;
+}
     
 int main(int argc, char* argv[]) {    
 	int create_socket, new_socket;
@@ -17,9 +52,16 @@ int main(int argc, char* argv[]) {
 	char *buffer = malloc(bufsize);
 	struct sockaddr_in address;
 	int PORT = atoi(argv[1]);
-	
+
+	int i;
+	int sendimg;
+	char *path;
+	char getPath[256] = "./html";
+
 	FILE *fp;
 	char str[BUFSIZ -1] = {};
+
+	char *cont;
  	
 	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
@@ -35,31 +77,48 @@ int main(int argc, char* argv[]) {
 		perror("bind");
 		exit(1);
 	}
+
     	setsockopt(create_socket, SOL_SOCKET, SO_REUSEADDR, &optvalue, sizeof(optvalue));
     
-	while (1) {    
-	if (listen(create_socket, 10) < 0) {    
-		perror("server: listen");    
-		exit(1);    
+	while (1) {
+
+		if (listen(create_socket, 10) < 0) {    
+			perror("server: listen");    
+			exit(1);
+		}
+
+		if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) == -1) {    
+			perror("server: accept");
+			exit(1);
+		}
+
+		if (new_socket > 0){    
+			printf("The Client is connected...\n");
+		}
+ 
+		recv(new_socket, buffer, bufsize, 0);
+		printf("%s\n", buffer);
+
+// finding GET
+		strtok(buffer," ");
+		path = strtok(NULL," ");
+		
+		if(!strcmp(path,"/")){
+			fp = fopen("./html/index.html","r");
+			while(fgets(str,10000,fp))
+			{
+				write(new_socket, str, strlen(str));
+			}
+		}
+		else{
+			strcat(getPath,path);
+			sendimg = open(getPath, O_RDONLY);
+			sendfile(new_socket,sendimg,NULL,10000);
+			close(sendimg);
+		}
+
+		close(new_socket);
 	}
-	if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) == -1) {    
-		perror("server: accept");
-		exit(1);
-	}
-	if (new_socket > 0){    
-		printf("The Client is connected...\n");
-	}
-        
-	recv(new_socket, buffer, bufsize, 0);
-	printf("%s\n", buffer);
-	
-	fp = fopen("./html/index.html","r+");
-	while(fgets(str,100,fp))
-	{
-		write(new_socket, str, strlen(str));
-	}    
-      	close(new_socket);    
-   }
-   close(create_socket);    
-   return 0;    
+	close(create_socket);
+	return 0;
 }
