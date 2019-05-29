49386efa15b552af267f15159ed3e46a00831d67
NRK <49558826+notRoyKim@users.noreply.github.com>
2019-05-29 22:15:29 +0900

    Update main.c
    finally made it through to show the index,html and images..
    now I have to face the p_thread func..

--- main.c
+++ main.c
@@ -8,40 +8,52 @@
 #include <sys/un.h>
 #include <sys/sendfile.h>
 #include <fcntl.h>
+#include <pthread.h>
 
 // char* find_get_name (char *buf)
+
+/*
+void *threadfunc(void *vargp)
+{
+	int new_socket;
+	struct sockaddr_in address;
+	socklen_t addrlen;
+
+	if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) == -1)
+	{
+		perror("server: accept");
+		exit(1);
+	}
+
+	if (new_socket > 0)
+	{    
+		printf("The Client is connected...\n");
+	}
+
+}
+*/
 
 char* find_content_type (char *filename);
 
 void refered(int ns, char* filename);
     
 int main(int argc, char* argv[]) {    
+	struct sockaddr_in address;
 	int create_socket, new_socket;
 	int optvalue = 1;
-
 	socklen_t addrlen;
-
 	int bufsize = 1024;
-	char filetype[256] = {};
-	FILE *fp;
-	struct stat filestat;
-	int filesiz;
-	int fd;
 
-	char *buffer = malloc(bufsize);
-	struct sockaddr_in address;
+	pthread_t tid;
 
 	int PORT = atoi(argv[1]);
-
 	int i;
-	int sendimg;
-	char *path;
-	char getPath[256] = "./html";
 
-	char str[BUFSIZ -1] = {};
+	char getPath[512] = {};
+	char str[BUFSIZ] = {};
+	char *buffer = malloc(bufsize);
+	char path[512] = {};
 
-	char *cont;
- 	
 	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1)
 	{
 		perror("socket");
@@ -51,92 +63,58 @@ int main(int argc, char* argv[]) {
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = inet_addr("0.0.0.0");
 	address.sin_port = htons(PORT);
-    
+
 	if (bind(create_socket, (struct sockaddr *) &address, sizeof(address)) == -1){    
 		perror("bind");
 		exit(1);
 	}
 
     	setsockopt(create_socket, SOL_SOCKET, SO_REUSEADDR, &optvalue, sizeof(optvalue));
-    
-	while (1) {
 
-		if (listen(create_socket, 10) < 0) {    
+	while (1)
+	{
+		if (listen(create_socket, 10) < 0)
+		{    
 			perror("server: listen");    
 			exit(1);
 		}
 
-		if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) == -1) {    
+		if ((new_socket = accept(create_socket, (struct sockaddr *) &address, &addrlen)) == -1)
+		{    
 			perror("server: accept");
 			exit(1);
 		}
-
-		if (new_socket > 0){    
+		if (new_socket > 0)
+		{    
 			printf("The Client is connected...\n");
 		}
- 
+		strcpy(getPath,"");
+		strcpy(getPath,argv[2]);
+		
+		
 		recv(new_socket, buffer, bufsize, 0);
 		printf("%s\n", buffer);
 
 // finding GET
 		strtok(buffer," ");
-		path = strtok(NULL," ");
-		printf("%s",path);
-
-//file exist check (in test, this does not need for speed up. because we already have all test case)
-		strcat(getPath,path);
-		fp = fopen(getPath,"r");
-
-		fd = open (getPath, O_RDONLY);
-		if( fstat(fd, &filestat) < 0)
-			printf ("Error in measuring the size of the file");
-//filetype saved
-		strcpy(filetype,find_content_type(path));
-
-//if file does not exist, or 
-		if(strcmp(path,"/") == 0)
+		strcat(getPath,strtok(NULL," "));
+		if(strcmp(getPath,"./html/favicon.ico") == 0)
 		{
-			fclose(fp);
-
-			fp = fopen("./html/index.html","r");
-			printf("%ld\n",filestat.st_size);
-			while(fgets(str,10000,fp))
-			{
-				write(new_socket, str, strlen(str));
-			}
+			write(new_socket,"HTTP/1.1 404 Not Found\r\n",26);
+			close(new_socket);
 		}
-		else if(strcmp(path,"/index.html") == 0)
-		{
-			fclose(fp);
-			fp = fopen("./html/index.html","r");
-
-			while(fgets(str,10000,fp))
-			{
-				write(new_socket, str, strlen(str));
-			}
-
-		}
-		else{
-			sendimg = open(getPath, O_RDONLY);
-			sendfile(new_socket,sendimg,NULL,10000);
-			close(sendimg);
-		}
-
-
-		close(new_socket);
+		else
+			refered(new_socket,getPath);
 	}
 	close(create_socket);
 	return 0;
 }
 
 char* find_content_type (char *filename) {
-	char *p;
-	int i;
 	char buf1[256];
 	char *buf2;
 	char *token;
 
-	p = (char *)malloc(30);
 	strcpy(buf1, filename);
 
 	token = strtok(buf1, ".");
@@ -157,11 +135,48 @@ char* find_content_type (char *filename) {
 	else
 		strcpy(buf2, "Content-Type: application/octet-stream \r\n");
 
-	p = buf2;
-	//printf("%s\n", p);
-	return p;
+	return buf2;
 }
 
-void refered(int ns, char* filename)
-{
+void refered(int ns, char* filename) {
+	struct stat filestat;
+	FILE *fp;
+	int fd;
+	char header_buff [2048];
+	char file_buff [1000000] = {};
+	char filesize[8];
+	char filetype[512] = {};
+
+	strcpy(filetype,find_content_type(filename));
+
+	if(((fd = open(filename, O_RDONLY)) < -1) || (fstat(fd, &filestat) < 0))
+	{
+		printf("Error in measuring the size of the file\n");
+		exit(1);
+	}
+	else
+		sprintf(filesize, "%zd", filestat.st_size);
+
+	if((fp = fopen(filename,"r")) ==  NULL)
+	{
+		printf("can't open file\n");
+	}
+
+	if(fp == NULL)
+	{
+		write(ns,"HTTP/1.1 404 Not Found\r\n",26);
+	}
+	else if(fp != NULL)
+	{
+		strcpy (header_buff, "HTTP/1.1 200 OK\r\nContent-Length: ");
+		strcat (header_buff, filesize);
+		strcat (header_buff, "\r\n");
+		strcat (header_buff, filetype);
+		strcat (header_buff, "Connection: keep-alive\r\n\r\n");
+		fread (file_buff, sizeof(char), filestat.st_size + 1, fp);
+		write (ns, header_buff, strlen(header_buff));
+		write (ns, file_buff, filestat.st_size);
+	}
+	fclose(fp);
+	close (ns);
 }
